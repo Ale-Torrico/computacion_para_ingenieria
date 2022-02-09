156bbda4cd304042e2974cea3f9d87f872b46903
Ale-Torrico <alekeyto8@gmail.com>
2022-02-09 08:45:17 -0400

    add resp2

--- examen/p2/p2.py
+++ examen/p2/p2.py
@@ -5,3 +5,18 @@ Created on Wed Feb  9 07:33:20 2022
 @author: AMD
 """
 
+# Dada la lista [10,20,30,10,5, 1, 3, 5, 4] separar en dos listas 
+# una lista debe contener solo los pares y la segunda lista solo debe contener los impares
+lista = [10,20,30,10,5, 1, 3, 5, 4]
+Pares=[]
+Impares=[]
+for num in lista:
+    if num % 2 == 0:
+    
+        Pares.append(num)
+    else:
+        Impares.append(num)
+
+print(lista)
+print(Pares)
+print(Impares)
