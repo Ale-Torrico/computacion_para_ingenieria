309990e9c30614b27a3b2488dacddc9221e82d88
Ale-Torrico <alekeyto8@gmail.com>
2022-02-09 09:27:13 -0400

    add resp1

--- examen/p1/p1.py
+++ examen/p1/p1.py
@@ -5,4 +5,15 @@ Created on Wed Feb  9 07:29:53 2022
 @author: AMD
 """
 
-print("hola mundo")
+#Dada una cadena verificar que sea palindroma
+comparar, aux = 0, 0
+texto = input("Ingrese una palabra: ")
+#metodo reversed permite invertir una lista sin modificarla 
+for ind in reversed(range(0, len(texto))):
+  if texto[ind].lower() == texto[aux].lower():
+    comparar += 1
+  aux += 1
+if len(texto) == comparar:
+  print("es palindromo")
+else:
+  print("no es palindromo")
