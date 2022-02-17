5481232e0b2dc433b0b307b42ea12f96d51bec30
Ale-Torrico <alekeyto8@gmail.com>
2022-02-17 09:11:51 -0400

    install and testk tk gui for python

---
+++ clase 8/test_tk.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 08:35:44 2022
+
+@author: AMD
+"""
+
+#clase 8/test_tk.py
+
+import tkinter
+
+tkinter._test()
--- examen/p3/p3.py
+++ examen/p3/p3.py
@@ -5,3 +5,28 @@ Created on Wed Feb  9 07:43:59 2022
 @author: AMD
 """
 
+#Dado el archivo estudiantes.txt y notas.txt unir en un solo archivo
+# llamado primer_parcial.txt que contendra los nombre seguido de su nota.
+f = open("estudiantes.txt", "r")
+g = open ("notas.txt", "r")
+c = open('texto 3.txt', 'w')
+
+while(True):
+    linea = f.readline()
+    linea2 = g.readline()
+    
+    if not linea:
+   
+        break
+    c.write(linea+ linea2 )
+try:
+    # Procesamiento para escribir en el fichero
+    c.write(linea)
+finally:
+ 
+    c.close()
+       
+f.close()
+g. close()
+
+
