d7300f6c1cf73ce95c3e462c294ca50507b3c7eb
Ale-Torrico <alekeyto8@gmail.com>
2022-02-18 00:27:04 -0400

    iclude multiplicadores

---
+++ clase 1/multiplicadores.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 23:50:23 2022
+
+@author: AMD
+"""
+
+mult = 1
+
+list = (1, 2, 3)
+
+for i in list:
+    print (f'valores de i {i}')
+    mult = mult * i
+
+print (f'la multiplicacion total de 1 al 3 es {mult}')
